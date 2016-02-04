41f1c69bb1b66151c6354e447ca9cf6d03797b71
Mathieu Lagrange <mathieu.lagrange@cnrs.fr>
2016-02-04 16:30:01 +0100

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# researchWebSite
+# explanesWeb
+Explanes presentation website
+
+C'est de l'angular relativement bateau, juste 2 choses a noter :
+
+1. pour simplifier la gestion, le contenu est reparti dans des fichiers fragments HTML dnas le repertoire "pages" ; ceux-ci sont inclus dans la page par la directive "ng-include". Pour que cela fonctionne, il faut acceder le site par http -- cela ne marchera pas en acces local "file". Donc il faut soit avoir un serveur local, soit avoir une version en ligne.
+
+2. Pour que ce soit un peu dynamique, j'ai fait des transitions entre tabs par "slides", ce qui rend le code un peu plus tordu que du simple "affiche / cache ce panneau".
+
+Pour la partie slideshow / profil en haut, je ne savais pas ce que tu voulais mettre, donc ce'st juste un placeholder.
+
+Style, couleurs, etc., sont bien sur a revoir.
+
+toto
---
+++ pageController.js
@@ -0,0 +1,41 @@
+app.controller("PageController", function($scope, $timeout, $interval) {
+
+    $scope.panels = [
+	{"name":"Research", "page":"pages/research.html", "anchor":"investigation"},
+	{"name":"Reproducible Research", "page":"pages/reproducible.html", "anchor":"reproducible"},
+	{"name":"ExpLanes (EXPerimental PLANning for Engineering and Science)", "page":"pages/explanes.html", "anchor":"explanes"},
+	{"name":"Contributions", "page":"pages/contributions.html", "anchor":"contributions"},
+	{"name":"Publications", "page":"web/pages/publications.html", "anchor":"publications"},
+   ];
+
+    var scrollSpeedMult = 0;
+    var progressiveScrollTo = function (y, lastY) {
+	if (lastY == window.scrollY) return;
+	scrollSpeedMult++;
+	var delta = Math.round(scrollSpeedMult*(y-window.scrollY)*scrollSpeedMult/150);
+	if (Math.abs(delta) < 5) window.scrollTo(0, y);
+	else {
+	    lastY = window.scrollY;
+	    window.scrollTo(0, window.scrollY+delta);
+	    setTimeout(progressiveScrollTo.curry(y, lastY), 50);
+	}
+    }
+    
+    $scope.scrollTo = function (id) {
+	var coords = pageCoordinates(document.getElementById(id));
+	scrollSpeedMult = 3;
+	//window.scrollTo(0, coords.y - 50);
+	progressiveScrollTo(coords.y - 50);
+    }
+
+    $scope.isAbsolute = true;
+    $scope.absoluteStyle = {position:"absolute", top: "100px", left:"10px"};
+    $scope.fixedStyle = {position:"fixed", top: "0px", left:"10px"};
+    window.addEventListener("optimizedScroll", function () {
+	if (window.scrollY > 180 && $scope.isAbsolute)
+	    $scope.$apply("isAbsolute = false");
+	else if (window.scrollY <= 180 && !$scope.isAbsolute)
+	    $scope.$apply("isAbsolute = true");
+    });
+    
+});
